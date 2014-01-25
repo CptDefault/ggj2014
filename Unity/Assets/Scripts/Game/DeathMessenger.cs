1c510b91e66c5d92f0e3e560dc6a24b486b67d61
Winston tang <winstontang@live.com>
2014-01-25 22:12:00 +1100

    Fixed muzzle flash

---
+++ Unity/Assets/Scripts/Game/DeathMessenger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathMessenger : MonoBehaviour {
+
+
+	private List<string> verbs;
+
+	public Player receiver;
+	public Player sender;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+
+
+
+		//player 1 message
+		if(receiver.playerNumber == 1)
+		{
+			
+		}
+	}
+}
