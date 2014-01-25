ffc85d06cc916d6a0461d46de4df470c965b4aca
Winston tang <winstontang@live.com>
2014-01-25 23:47:45 +1100

    Death message

--- Unity/Assets/Scripts/Player/RespawnCamera.cs
+++ Unity/Assets/Scripts/Player/RespawnCamera.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class RespawnCamera : MonoBehaviour {
-
+    string _diedMessage;
+    Rect _diedMessageRect; 
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +14,10 @@ public class RespawnCamera : MonoBehaviour {
 	
 	}
 
-    public void Activate(Player player)
+    public void Activate(Player player, string verb, string sender, Rect dr)
     {
+        _diedMessageRect = dr;
+          _diedMessage = "YOU GOT " + verb + " BY " + sender;
         StartCoroutine(ActiveCoroutine(player));
     }
 
@@ -23,12 +26,18 @@ public class RespawnCamera : MonoBehaviour {
         camera.enabled = true;
         player.gameObject.SetActive(false);
 
-
         yield return new WaitForSeconds(3);
 
         camera.enabled = false;
+        _diedMessage = null;
 
         player.gameObject.SetActive(true);
         player.Respawn();
+    }
+
+    void OnGUI()
+    {
+        if(_diedMessage != null)
+            GUI.Box(_diedMessageRect, _diedMessage, DeathMessenger.Instance.messageSkin.GetStyle("Message"));
     }
 }
